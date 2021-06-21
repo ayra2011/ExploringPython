37bb9b6cea2ce33cfd396982df92d528989b121d
ayra2011 <ayrasingh2011@gmail.com>
2021-06-21 11:57:57 +0800

    draw square program updated

--- src/DrawSquareInAFunction.py
+++ src/DrawSquareInAFunction.py
@@ -10,49 +10,26 @@ def square():
         t.left(90)
 
 
-for index in range(3):
-    square()
-    t.penup()
-    t.forward(20)
-    t.pendown()
+square_count = 3
+circle_count = 1
 
-for index in range(1):
-    t.penup()
-    t.forward(5)
-    t.pendown()
-    t.circle(5)
-    t.penup()
-    t.forward(20)
-    t.pendown()
+for x in range(4):
 
-for index in range(2):
-    square()
-    t.penup()
-    t.forward(20)
-    t.pendown()
+    for i in range(square_count):
+        square()
+        t.penup()
+        t.forward(20)
+        t.pendown()
+        square_count -= 1
 
-for d in range(2):
-    t.penup()
-    t.forward(5)
-    t.pendown()
-    t.circle(5)
-    t.penup()
-    t.forward(20)
-    t.pendown()
-
-for index in range(1):
-    square()
-    t.penup()
-    t.forward(20)
-    t.pendown()
-
-for n  in range(3):
-    t.penup()
-    t.forward(5)
-    t.pendown()
-    t.circle(5)
-    t.penup()
-    t.forward(20)
-    t.pendown()
+    for i in range(circle_count):
+        t.penup()
+        t.forward(5)
+        t.pendown()
+        t.circle(5)
+        t.penup()
+        t.forward(20)
+        t.pendown()
+        circle_count += 1
 
 turtle.done()
