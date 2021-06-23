b32ea20c6d738a41c9c55b6af183853ff484c809
ayra2011 <ayrasingh2011@gmail.com>
2021-06-23 13:22:43 +0800

    Fixed Flag.py to work with new Tenor.gif location

--- src/DrawSquareInAFunction.py
+++ src/DrawSquareInAFunction.py
@@ -1,7 +1,11 @@
 import turtle
+import random
 t = turtle.Turtle()
 
+turtle.bgcolor('black')
+t.pensize(3)
 t.speed(0)
+colour_list = ['orange', 'pink', 'purple', 'light blue', 'red', 'magenta', 'cyan', 'yellow', 'violet']
 
 
 def square():
@@ -10,26 +14,60 @@ def square():
         t.left(90)
 
 
-square_count = 3
-circle_count = 1
+def square_circle_train():
+    square_count = 3
+    circle_count = 1
+    for x in range(3):
 
-for x in range(4):
+        t.begin_fill()
+        turtle.fillcolor()
+        t.end_fill()
 
-    for i in range(square_count):
-        square()
-        t.penup()
-        t.forward(20)
-        t.pendown()
+        for i in range(square_count):
+            random_colour_index = random.randint(0, 8)
+            t.pencolor(colour_list[random_colour_index])
+            square()
+            t.penup()
+            t.forward(20)
+            t.pendown()
         square_count -= 1
 
-    for i in range(circle_count):
-        t.penup()
-        t.forward(5)
-        t.pendown()
-        t.circle(5)
-        t.penup()
-        t.forward(20)
-        t.pendown()
+        for i in range(circle_count):
+            random_colour_index = random.randint(0, 8)
+            t.pencolor(colour_list[random_colour_index])
+            t.penup()
+            t.forward(5)
+            t.pendown()
+            t.circle(5)
+            t.penup()
+            t.forward(20)
+            t.pendown()
         circle_count += 1
+
+
+for index in range(4):
+    square_circle_train()
+    t.penup()
+    t.right(90)
+    t.forward(20)
+    t.right(90)
+    t.forward(13)
+    t.pendown()
+
+    t.penup()
+    t.right(90)
+    t.forward(40)
+    t.left(90)
+    t.forward(13)
+    t.pendown()
+    square_circle_train()
+    t.penup()
+    t.right(90)
+    t.forward(20)
+    t.right(90)
+    t.forward(13)
+    t.pendown()
+
+t.ht()
 
 turtle.done()
