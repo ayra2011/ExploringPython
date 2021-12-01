7b48734d587ef431d5eaddb4bfa8d718f8391a57
ayra2011 <ayrasingh2011@gmail.com>
2021-12-01 12:42:13 +0800

    still working on list

---
+++ src/fungtions.py
@@ -0,0 +1,19 @@
+# name1 = input('Enter your name: ')
+# greeting1 = 'how are you doing today, did u have a good day'
+#
+#
+# def greet_person(name, greeting):
+#     print(greeting, name)
+#
+
+# greet_person(name1, greeting1)
+
+
+def sum_of_numbers(n1, n2):
+    n1 = 18
+    n2 = 29
+    sn = n1 + n2
+    return sn, n1, n2
+
+
+sn = (sum_of_numbers(222, 728))
