c855b159375230a65d8ce0c239668242c45fd326
ayra2011 <ayrasingh2011@gmail.com>
2021-12-21 12:26:19 +0800

    quitcion!!

--- src/listPrastice.py
+++ src/listPrastice.py
@@ -1,16 +1,16 @@
 userDefinedList = []
+userDefinedListRevers = []
 numOfTimesProgramWillRun = int(input("Enter number of times you want program to run:  "))
-
-num = len(userDefinedList) - 1
-num1 = numOfTimesProgramWillRun // 2
 for index in range(numOfTimesProgramWillRun):
     numInList = int(input("Enter number you want the list to have:  "))
     userDefinedList.append(numInList)
 
+num = 0
 
-for i in range(num1):
-    numInList = userDefinedList[num]
-    num -= 2
-    userDefinedList.append(numInList)
+for i in range(numOfTimesProgramWillRun // 2):
+    numInListRevers = userDefinedList[num]
+    num += 2
+    userDefinedListRevers.append(numInListRevers)
 
 print(userDefinedList)
+print(userDefinedListRevers)
