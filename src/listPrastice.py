3af8e72bcd895dd4e99878e45acf6da6617e3da5
soumyarai2050 <soumyarai2050@gmail.com>
2021-12-21 12:56:03 +0800

    Avoid duplicate code

--- src/listPrastice.py
+++ src/listPrastice.py
@@ -6,18 +6,16 @@ for index in range(numOfTimesProgramWillRun):
     userDefinedList.append(numInList)
 
 num = 0
+
 if numOfTimesProgramWillRun % 2 >= 1:
     timesToLoop = numOfTimesProgramWillRun // 2 + 1
-    for i in range(timesToLoop):
-        numInListRevers = userDefinedList[num]
-        num += 2
-        userDefinedListRevers.append(numInListRevers)
 else:
     timesToLoop = numOfTimesProgramWillRun // 2
-    for i in range(timesToLoop):
-        numInListRevers = userDefinedList[num]
-        num += 2
-        userDefinedListRevers.append(numInListRevers)
+
+for i in range(timesToLoop):
+    numInListRevers = userDefinedList[num]
+    num += 2
+    userDefinedListRevers.append(numInListRevers)
 
 print(userDefinedList)
 print(userDefinedListRevers)
