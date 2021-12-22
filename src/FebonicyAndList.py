3856583e55d3af32e4dd2a93229ed382aa78c2ec
ayra2011 <ayrasingh2011@gmail.com>
2021-12-22 12:13:23 +0800

    pls see my program done implementing

--- src/FebonicyAndList.py
+++ src/FebonicyAndList.py
@@ -1,3 +1,20 @@
 # Write a program to accept user provided names in a list
 # print that list
 # print again the list - but this time print names that are on indexes aligned with Fibonacci series numbers
+numOfTimesMyProgramWillRun = int(input("Enter the number of times the program should run:  "))
+userDifiendList = []
+
+for index in range(numOfTimesMyProgramWillRun):
+    userDifiendNum = int(input("Enter any number:  "))
+    userDifiendList.append(userDifiendNum)
+
+print(userDifiendList)
+
+num1 = userDifiendList[0]
+num2 = userDifiendList[1]
+
+for i in range(numOfTimesMyProgramWillRun):
+    num3 = num1 + num2
+    print(num3)
+    num1 = num2
+    num2 = num3
