4d86967e5a274054e111fa4b06e491c08563546a
soumyarai2050 <soumyarai2050@gmail.com>
2021-12-22 11:48:24 +0800

    Defined question

--- src/FebonicyAndList.py
+++ src/FebonicyAndList.py
@@ -1,1 +1,3 @@
-#
+# Write a program to accept user provided names in a list
+# print that list
+# print again the list - but this time print names that are on indexes aligned with Fibonacci series numbers
