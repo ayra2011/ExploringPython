8104de5e2a075a350e5a9d56dc252b329dbe452b
Ayra <soumyarai2050@gmail.com>
2023-07-17 22:16:49 +0800

    py-auto-gui experiment

--- Experiments/pyautoui-sample.py
+++ Experiments/pyautoui-sample.py
@@ -3,17 +3,27 @@ import time
 import sys
 from datetime import datetime
 pyautogui.FAILSAFE = False
-
-x: int = 0
+sleep_interval = 8
+start_row = 170
+end_row = 1000
+start_col = 1630
+column_decrement_size = 130
+row_increment_size = 50
+row: int = start_row
+column: int = start_col
 while True:
-    time.sleep(5)
-    x += 100
-    pyautogui.moveTo(1620, x+400)
+    time.sleep(sleep_interval)
+    pyautogui.moveTo(column, row)
     pyautogui.click()
-    time.sleep(5)
-    if x > 1900:
-        x = 0
-pyautogui.moveTo(1, 1)
+    time.sleep(sleep_interval)
+    row += row_increment_size
+    if row >= end_row:
+        row = start_row
+        column -= column_decrement_size
+    if column <= (start_col - (4 * column_decrement_size)):
+        column = start_col
+
+    # pyautogui.moveTo(1, 1)
 
 # for i in range(0, 3):
 #     pyautogui.press("shift")
